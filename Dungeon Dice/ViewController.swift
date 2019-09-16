21f9b1bc77eeec2f8a4aceb4032552c3d50c04fd
yinzhema <yma9826@gmail.com>
2019-09-16 17:33:59 -0400

    version 1 complete

--- Dungeon Dice/ViewController.swift
+++ Dungeon Dice/ViewController.swift
@@ -10,11 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceNumberLabel: UILabel!
+    var number=0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    func giveNumber(sideNumber:Int) {
+        number=Int.random(in:1...sideNumber)
+        diceNumberLabel.text=String(number)
+    }
+    
+    @IBAction func fourSidePressed(_ sender: UIButton) {
+        giveNumber(sideNumber: 4)
+    }
+    
+    @IBAction func sixSidePressed(_ sender: UIButton) {
+        giveNumber(sideNumber: 6)
+    }
+    
+    
+    @IBAction func eightSidePressed(_ sender: UIButton) {
+        giveNumber(sideNumber: 8)
+    }
+    
+    @IBAction func tenSidePressed(_ sender: UIButton) {
+        giveNumber(sideNumber: 10)
+    }
+    
+    @IBAction func twelveSidePressed(_ sender: UIButton) {
+       giveNumber(sideNumber: 12)
+    }
+    
+    @IBAction func twentySidePressed(_ sender: UIButton) {
+       giveNumber(sideNumber: 20)
+    }
+    
+    @IBAction func hundredSidePressed(_ sender: UIButton) {
+        giveNumber(sideNumber: 100)
+    }
+    
+    
 
 }
 
